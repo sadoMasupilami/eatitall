123ff92163efc2b47494396d9eeef4dbbb58ee28
Michael Klug <michiklug85@gmail.com>
2020-06-19 15:09:17 +0200

    recycler view wip

--- app/src/main/java/at/fhjoanneum/eatitall/ui/TestActivity.java
+++ app/src/main/java/at/fhjoanneum/eatitall/ui/TestActivity.java
@@ -8,23 +8,29 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
 import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.ParsedRequestListener;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import at.fhjoanneum.eatitall.R;
 import at.fhjoanneum.eatitall.model.Meal;
 import at.fhjoanneum.eatitall.model.MealContainer;
+import at.fhjoanneum.eatitall.ui.adapter.MealAdapter;
 
 public class TestActivity extends AppCompatActivity {
 
     TextView textView;
     EditText editText;
-
+    RecyclerView recyclerView;
+    MealAdapter mealAdapter;
+    List<Meal> myMeals;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,10 +40,24 @@ public class TestActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.test_textView);
         editText = findViewById(R.id.test_editText);
+        recyclerView = findViewById(R.id.test_recycler);
+
+        myMeals = new ArrayList<>();
+        myMeals.add(new Meal("Hendl Klug", "www.hendl.at", "123"));
+        myMeals.add(new Meal("Hendl Klug", "www.hendl.at", "123"));
+        myMeals.add(new Meal("Hendl Klug", "www.hendl.at", "123"));
+        mealAdapter = new MealAdapter(this, myMeals);
+        recyclerView.setAdapter(mealAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     public void search(View view) {
         String ingredient = editText.getText().toString();
+
+        if (ingredient.equals("")) {
+            Toast.makeText(getApplicationContext(), "Please enter an ingredient to search for", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
         AndroidNetworking.get("https://www.themealdb.com/api/json/v1/1/filter.php")
                 .addQueryParameter("i", ingredient)
@@ -46,19 +66,22 @@ public class TestActivity extends AppCompatActivity {
                 .getAsObject(MealContainer.class, new ParsedRequestListener<MealContainer>() {
                     @Override
                     public void onResponse(MealContainer mealContainer) {
-                        List<Meal> myMeals = mealContainer.getMeals();
+                        myMeals = mealContainer.getMeals();
                         if (myMeals != null) {
                             textView.setText(myMeals.get(0).toString());
                             Toast.makeText(getApplicationContext(), "recipes found: " + myMeals.size(), Toast.LENGTH_SHORT).show();
+                            mealAdapter = new MealAdapter(getApplicationContext(), myMeals);
+                            recyclerView.setAdapter(mealAdapter);
+                            recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
                         } else {
                             Toast.makeText(getApplicationContext(), "no recipes found", Toast.LENGTH_SHORT).show();
+                            textView.setText("");
                         }
                     }
 
                     @Override
                     public void onError(ANError anError) {
                         Toast.makeText(getApplicationContext(), "ERROR: " + anError.getMessage(), Toast.LENGTH_LONG).show();
-                        textView.setText("0");
                     }
                 });
     }
---
+++ app/src/main/java/at/fhjoanneum/eatitall/ui/adapter/MealAdapter.java
@@ -0,0 +1,56 @@
+package at.fhjoanneum.eatitall.ui.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import at.fhjoanneum.eatitall.R;
+import at.fhjoanneum.eatitall.model.Meal;
+
+public class MealAdapter extends RecyclerView.Adapter<MealAdapter.MealViewHolder> {
+
+    Context context;
+    List<Meal> meals;
+
+    public MealAdapter(Context context, List<Meal> meals) {
+        this.context = context;
+        this.meals = meals;
+    }
+
+    @NonNull
+    @Override
+    public MealViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.meal_item, parent, false);
+        return new MealViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MealViewHolder holder, int position) {
+        holder.name.setText(meals.get(position).getStrMeal());
+        holder.id.setText(meals.get(position).getIdMeal());
+    }
+
+    @Override
+    public int getItemCount() {
+        return meals.size();
+    }
+
+    public class MealViewHolder extends RecyclerView.ViewHolder {
+
+        TextView name, id;
+
+        public MealViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.tvMealName);
+            id = itemView.findViewById(R.id.tvMealId);
+        }
+    }
+}
